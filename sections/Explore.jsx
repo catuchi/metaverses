5be967c415d6572f0acb094f4f846736ca32c51a
Chibu <atuchibueze@gmail.com>
2022-12-23 16:02:58 -0500

    add title to explore

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,33 @@
-'use client';
+"use client";
+
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { ExploreCard, TitleText, TypingText } from "../components";
+import { exploreWorlds } from "../constants";
 
 const Explore = () => (
-  <section>
-    Explore section
+  <section id="explore" className={`${styles.paddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title={"| The World"} textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Choose the world you want <br className="md:block hidden" /> to
+            explore
+          </>
+        }
+        textStyles={"text-center"}
+      />
+    </motion.div>
   </section>
 );
 
