b933d0eb59411769117a8eb99237a6e2c6e369ad
Chibu <atuchibueze@gmail.com>
2022-12-24 20:54:22 -0500

    create and style StartSteps component

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -26,6 +26,25 @@ const GetStarted = () => (
           className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title={"| How Metaverses Works"} />
+        <TitleText
+          title={
+            <>
+              Get started <br className="md:block hidden" /> with just a few{" "}
+              <br className="md:block hidden" /> clicks
+            </>
+          }
+        />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps key={feature} number={index + 1} text={feature} />
+          ))}
+        </div>
+      </motion.div>
     </motion.div>
   </section>
 );
